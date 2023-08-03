cb54c87c5e0850b5360928cbe5b440c966e28a7a
GUNA <132567440+GUNAVIJAY01@users.noreply.github.com>
2023-08-03 10:32:49 +0530

    Update promisetask.js

--- promisetask.js
+++ promisetask.js
@@ -1,31 +1,30 @@
-// function pr(a){
-// 	return new Promise((res,rej)=>{
-// 		if(a%2==0){
-// 				res(a)		
-// 		}
-// 		else{
-// 			rej(a)
-// 		}
-// 		})
-// }
+ function pr(a){
+ 	return new Promise((res,rej)=>{
+ 		if(a%2==0){
+ 				res(a)		
+ 		}
+ 		else{
+ 			rej(a)
+ 		}
+ 		})
+ }
 
 
 
 
 
-// pr(15).then((x)=>{
+ pr(15).then((x)=>{
 
-// 		console.log(`${x} number is Even number`)
-// })
-// .catch((y)=>{
-// 		console.log(`${y} number is Odd number`)	
-// })
+ 		console.log(`${x} number is Even number`)
+ })
+ .catch((y)=>{
+ 		console.log(`${y} number is Odd number`)	
+ })
 		
 		// amstrong
 
 function pr(n){
 // let x=371;
-
 let y=n;
 let z=y;
 let sum=0;
@@ -64,32 +63,31 @@ pr(407)
 		console.log(`${y} number is not amstrong number`)	
 })
 
-// 				// palindrome
-// function pr(n){
-// let y=n;
-// let sum=0;
-// while(n!=0){
+ 				// palindrome
+ function pr(n){
+ let y=n;
+ let sum=0;
+ while(n!=0){
 
-// 	let rem=n%10;
-// 		n=(n-rem)/10;	
-// 	sum=(sum*10)+rem;
-// 	}
+ 	let rem=n%10;
+ 		n=(n-rem)/10;	
+ 	sum=(sum*10)+rem;
+ 	}
 
-// 		// let sum=371;
-// 	return new Promise((res,rej)=>{
-// 		if(sum==y){
-// 				res(y)		
-// 		           }
-// 		else{
-// 			rej(y)
-// 		    }
-// 		})
-// }
+ 		// let sum=371;
+ 	return new Promise((res,rej)=>{
+ 		if(sum==y){
+ 				res(y)		
+ 		           }
+ 		else{
+ 			rej(y)
+ 		    }
+ 		})
+ }
+ pr(373).then((q)=>{
 
-// pr(373).then((q)=>{
-
-// 		console.log(`${q} number is palindrome number`)
-// })
-// .catch((w)=>{
-// 		console.log(`${w} number is not palindrome number`)	
-// })
+ 		console.log(`${q} number is palindrome number`)
+ })
+ .catch((w)=>{
+ 		console.log(`${w} number is not palindrome number`)	
+ })
